52a8045ac3d6ed1484ff6ec8a6111c1d66e56445
francineleey-sudo <francineleey@gmail.com>
2025-08-11 22:56:16 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -6,12 +6,12 @@ import 'bolondroclass.dart';
 import 'malinaoclass.dart';
 
 void main() {
-  var yusoyaclass = Yusoya('Francine Lee Yusoya', '20');
+  var yusoyaclass = Yusoya('Francine Lee Yusoya', 20);
   yusoyaclass.describe();
-  var batisclass = Batis('Yan Batis Pelande', '20');
+  var batisclass = Batis('Yan Batis Pelande', 20);
   batisclass.describe();
-  var bolondroclass = Bolondro('Jim Bolondro', '21');
+  var bolondroclass = Bolondro('Jim Bolondro', 21);
   bolondroclass.describe();
-  var malinaoclass = Malinao('Baby Jane Mujar Malinao', '21');
+  var malinaoclass = Malinao('Baby Jane Mujar Malinao', 21);
   malinaoclass.describe();
 }
